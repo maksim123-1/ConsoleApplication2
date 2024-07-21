ff3d7753c98e98d45e184607d703457a6f89a738
Maksim <Maksim@DESKTOP-BUUKA3P>
2024-07-21 19:07:07 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct user
+{
+    int id;
+    char name[255];
+};
+
+int main()
+{
+    user a;
+    a.id = 1;
+    strcpy(a.name, "Максим");
+    user b;
+    b.id = 2;
+    strcpy(b.name, "Алекс");
+    ofstream fo;
+    ifstream fi;
+    fo.open("output.dat", ios::ate);
+    fo.write((char*)&a, sizeof(a));
+    fo.write((char*)&b, sizeof(b));
+    fo.close();
+    user c;
+    fi.open("output.dat", ios::in);
+    while (fi.read((char*)&c, sizeof(c)))
+        cout << c.id << " " << c.name << endl;
+    fi.close();
+    return 0;
+}
